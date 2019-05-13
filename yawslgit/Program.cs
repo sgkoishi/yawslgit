bf68a761ee6d48abdec512fb01862657b893274d
SGKoishi <sgkoishi@hotmail.com>
2019-05-13 13:36:34 -0700

    Convert WSL path back to Windows path

--- yawslgit/Program.cs
+++ yawslgit/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -17,17 +18,45 @@ namespace yawslgit
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
         private static readonly object _writeLock = new object();
+        private static readonly RegistryKey lxss = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Lxss");
+        public static readonly string SystemDrive = Path.GetPathRoot(Environment.SystemDirectory);
 
+        // Get all mount points of WSL as a Dict<Drive, MountPath>
+        public static Dictionary<string, string> Mounts = Process.Start(Start("cat /proc/mounts | grep :")).StandardOutput.ReadToEnd().Trim().Split('\n').ToDictionary(n => n.Split()[0], n => n.Split()[1]);
+
+        // Absolute path to WSL's file system (default distro), in Windows
+        public static string linuxBasePath = (string) lxss.OpenSubKey(lxss.GetValue("DefaultDistribution").ToString()).GetValue("BasePath");
+
+        /// <summary>
+        /// Convert a Windows absolute path to WSL (mounted) path
+        /// </summary>
+        /// <param name="path">String to convert</param>
+        /// <returns>Converted result</returns>
         private static string ToLinuxPath(string path)
         {
-            return char.IsLetter(path[0]) && path[1] == ':' ? $"/mnt/{char.ToLower(path[0])}{path.Substring(2)}".Replace("\\", "/") : path;
+            return char.IsLetter(path[0]) && path[1] == ':' ? (Mounts[path.Substring(0, 2)] + path.Substring(2)).Replace("\\", "/") : path;
         }
 
-        private static string ToWindowsPath(string path)
+        /// <summary>
+        /// Convert all WSL path in mounted drive into Windows path.
+        /// </summary>
+        /// <param name="log">String to convert</param>
+        /// <returns>Converted result, with "/" as path separator</returns>
+        private static string ToWindowsPath(string log)
         {
-            throw new NotImplementedException();
+            foreach (var item in Mounts)
+            {
+                log = log.Replace(item.Value, item.Key);
+            }
+            return log;
         }
 
+        /// <summary>
+        /// Convert a list of arguments to a string of command line.
+        /// https://docs.microsoft.com/en-us/cpp/cpp/parsing-cpp-command-line-arguments
+        /// </summary>
+        /// <param name="args">List of arguments passed to this program</param>
+        /// <returns>A string of command line with proper quotes and escape.</returns>
         private static string ToCommandLine(string[] args)
         {
             var result = new List<string>();
@@ -92,6 +121,24 @@ namespace yawslgit
             }
         }
 
+        /// <summary>
+        /// A helper function to execute command in WSL.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        private static ProcessStartInfo Start(string command)
+        {
+            return new ProcessStartInfo(SystemDrive + "Windows\\Sysnative\\wsl.exe")
+            {
+                UseShellExecute = false,
+                Arguments = command,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true
+            };
+        }
+
         private static void Main(string[] args)
         {
             var argsOnly = ToCommandLine(args.Select(ToLinuxPath).ToArray());
@@ -100,33 +147,20 @@ namespace yawslgit
                 // TortoiseGit workaround
                 // See https://gitlab.com/tortoisegit/tortoisegit/issues/3380
                 // Run "git status" will fix this problem - but why?
-                Process.Start(new ProcessStartInfo(@"C:\Windows\Sysnative\wsl.exe")
-                {
-                    UseShellExecute = false,
-                    Arguments = "git status",
-                    CreateNoWindow = true
-                }).WaitForExit();
+                Process.Start(Start("git status")).WaitForExit();
             }
             var token = new Random().Next();
             Log($"CL ({token}):\r\n\t{Environment.CommandLine}\r\n");
             Log($"Args ({token}):\r\n\t{string.Join("\r\n\t", args)}\r\n");
-            var psi = new ProcessStartInfo(@"C:\Windows\Sysnative\wsl.exe")
-            {
-                UseShellExecute = false,
-                Arguments = "git " + argsOnly,
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                RedirectStandardInput = true
-            };
+            var psi = Start("git " + argsOnly);
             Log($"Invoke ({token}):\r\n\t{psi.Arguments}\r\n");
-            Console.Error.Write($"Git command: {psi.Arguments}\n");
             var git = new Process() { StartInfo = psi };
             git.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
             {
                 var str = e?.Data; // ?.TrimEnd('\0');
                 if (!string.IsNullOrEmpty(str))
                 {
+                    str = ToWindowsPath(str);
                     Log($"Output ({token}):\r\n\t{str}\r\n");
                     Console.Out.Write(str + "\n");
                 }
@@ -136,6 +170,7 @@ namespace yawslgit
                 var str = e?.Data; // ?.TrimEnd('\0');
                 if (!string.IsNullOrEmpty(str))
                 {
+                    str = ToWindowsPath(str);
                     Log($"Error ({token}):\r\n\t{str}\r\n");
                     Console.Error.WriteLine(str + "\n");
                 }
